459ee68957e821921a922aae27e3b4a4151c515b
geeky4dev <geeky4dev@gmail.com>
2025-05-24 17:48:16 +0200

    Actualizar API para usar variable de entorno y agregar .env.production

--- frontend/src/services/solarAPI.js
+++ frontend/src/services/solarAPI.js
@@ -1,4 +1,5 @@
 export async function fetchSolarData(lat, lon) {
-  const res = await fetch(`http://localhost:5000/api/solar?lat=${lat}&lon=${lon}`);
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const res = await fetch(`${backendUrl}/api/solar?lat=${lat}&lon=${lon}`);
   return await res.json();
 }
